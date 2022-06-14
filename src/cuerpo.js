a3b4f925627213c955d13638e41f496efcdade55
Cami Romito <romitocamila5@gmail.com>
2022-06-14 18:29:17 -0300

    creando proyecto

---
+++ src/cuerpo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Cuerpo() {
+  return (
+    <div className="container">
+    
+      <img src="https://s1.eestatic.com/2019/03/04/ciencia/nutricion/nutricion_380722770_117009518_854x640.jpg" class="img-fluid" alt="..."></img>
+    </div>
+  );
+}
